2f04f65f20f4cb8397eb0f8f717e536ec6f7f4ba
Martín Albanesi <martinalbanesi89@gmail.com>
2022-10-01 14:01:47 -0300

    layered modularized project

--- app/src/main/java/com/practice/apirestapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/practice/apirestapp/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.practice.apirestapp
+package com.practice.apirestapp.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.practice.apirestapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
