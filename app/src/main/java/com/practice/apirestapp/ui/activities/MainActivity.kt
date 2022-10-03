3887a574496629f467086c9e2462500b090fb5c0
Martín Albanesi <martinalbanesi89@gmail.com>
2022-10-03 18:52:18 -0300

    created MainViewModel, ready to merge with koin branch for dependency injection modules

--- app/src/main/java/com/practice/apirestapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/practice/apirestapp/ui/activities/MainActivity.kt
@@ -3,6 +3,11 @@ package com.practice.apirestapp.ui.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.practice.apirestapp.R
+import com.practice.apirestapp.ui.viewmodels.MainViewModel
+import org.koin.android.compat.ScopeCompat.viewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+private val vm: MainViewModel by viewModel()
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/practice/apirestapp/ui/viewmodels/MainViewModel.kt
@@ -0,0 +1,19 @@
+package com.practice.apirestapp.ui.viewmodels
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.practice.apirestapp.data.repositories.BeerRepository
+import com.practice.apirestapp.data.repositories.beers.api.models.APIBeerModel
+import kotlinx.coroutines.launch
+
+class MainViewModel (private val beersList: BeerRepository) : ViewModel(){
+
+    private var myBeerList = MutableLiveData(listOf(APIBeerModel(0,"fill","fill","fill")))
+
+    suspend fun fill(){
+        viewModelScope.launch {
+            myBeerList.value = beersList.listBeers()
+        }
+    }
+}
